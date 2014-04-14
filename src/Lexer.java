2a4bc5157678e4af64a159802b6348c5646a529f
Brian To <bxt5647@rit.edu>
2014-04-13 23:27:49 -0400

    Add documentation to important things

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,11 +3,23 @@
 import java.util.Iterator;
 import java.util.regex.Matcher;
 
+/**
+ * Takes a {@link String} of Curl and produces a stream of {@link TokenData}.
+ * 
+ * This lexer works in two modes due to the oddities in tokenizing raw text.
+ * If in normal mode, tokens that represent tags (p, h, style) take precedence.
+ * However, in text mode, those text tokens aren't text tokens, but text. This
+ * lexer would interpret text as those more specific texts if not in the text
+ * context. Creating a separate text-only token state fixes this problem.
+ */
 public class Lexer implements Iterator<TokenData> {
 
 	private String input;
 	private LexerState state;
 	
+	/**
+	 * 
+	 */
 	private static final Token[] TEXT_RESTRICTED_TOKENS = {
 		Token.TEXT_ESCAPED, Token.TEXT_LITERAL, Token.BRACE_LEFT, Token.BRACE_RIGHT
 	};
@@ -32,6 +44,11 @@ public class Lexer implements Iterator<TokenData> {
 		throw new RuntimeException("Illegal state: not in NORMAL nor NEXT");
 	}
 	
+	/**
+	 * Grabs the next matching token in normal mode and returns the token as {@link TokenData}
+	 *  
+	 * @return {@link TokenData} for next token
+	 */
 	private TokenData nextNormal() {
 		for (Token token : Token.values()) {
 			Matcher matcher = token.matcher(this.input);
@@ -41,6 +58,7 @@ public class Lexer implements Iterator<TokenData> {
 				
 				this.input = this.input.replaceFirst(token.regex(), "");
 				
+				// Go into text mode if we see the text mode token
 				if (token.equals(Token.TEXT)) {
 					this.state = LexerState.TEXT;
 				}
@@ -51,7 +69,12 @@ public class Lexer implements Iterator<TokenData> {
 		
 		throw new IllegalStateException(String.format("No token matches next input %s", this.input));
 	}
-	
+
+	/**
+	 * Grabs the next matching token in text mode and returns the token as {@link TokenData}
+	 *  
+	 * @return {@link TokenData} for next token
+	 */
 	private TokenData nextText() {
 		for (Token token : TEXT_RESTRICTED_TOKENS) {
 			Matcher matcher = token.matcher(this.input);
@@ -61,6 +84,7 @@ public class Lexer implements Iterator<TokenData> {
 				
 				this.input = this.input.replaceFirst(token.regex(), "");
 				
+				// Get out of text mode and back into normal mode
 				if (token.equals(Token.BRACE_RIGHT)) {
 					this.state = LexerState.NORMAL;
 				}
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,13 @@ import java.io.FileReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * Entry point for program.
+ * 
+ * Parses given curl file and prints HTML to stdout.
+ * 
+ * @param args path to curl file
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -24,6 +31,14 @@ public class Main {
 		System.out.println(root.html());
 	}
 
+	/**
+	 * Returns {@link File} for file to curl.
+	 * 
+	 * Exits program if no argument or file not found
+	 * 
+	 * @param args main's arguments
+	 * @return {@link File} to curl file
+	 */
 	private static File inputFile(String[] args) {
 		if (args.length != 1) {
 			System.err.println("Bad arguments: make run CURL=<file.curl>");
@@ -40,6 +55,14 @@ public class Main {
 		return target;
 	}
 	
+	/**
+	 * Read file to string. :-)
+	 * 
+	 * Exits program if error in reading file.
+	 * 
+	 * @param input file to read
+	 * @return contents of file as {@link String}
+	 */
 	private static String readFileToString(File input) {
 		StringBuilder result = new StringBuilder();
 		String line = null;
